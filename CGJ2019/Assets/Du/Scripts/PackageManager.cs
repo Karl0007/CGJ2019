0e8facfb92726e5c6b6bf071f597c20f05ebba2e
karl07 <1474402804@qq.com>
2019-07-06 16:38:28 +0800

    3A V1

---
+++ CGJ2019/Assets/Du/Scripts/PackageManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PackageManager
+{
+
+    class ItemData
+    {
+        public int num;
+        public string name;
+        public string id;
+    }
+
+    private Dictionary<Item, ItemData> itemDic = new Dictionary<Item, ItemData>();
+    public enum Item
+    {
+        apple,
+        Axe,
+        piqiu,
+        zhurou,
+        key,
+    }
+
+    public void Add(Item i)
+    {
+        if (itemDic.ContainsKey(i))
+        {
+            itemDic[i].num += 1;
+        }
+        else
+        {
+            ItemData data = new ItemData();
+            data.num = 1;
+            data.name = "Item:" + i.ToString();
+            itemDic[i] = data;
+        }
+    }
+
+    public bool Delete(Item i)
+    {
+        if (itemDic.ContainsKey(i))
+        {
+            itemDic[i].num -= 1;
+
+            if(itemDic[i].num < 0)
+            {
+                return false;
+            }else
+            {
+                return true;
+            }
+            
+        }else
+        {
+            return false;
+        }
+    }
+
+    public bool Check(Item i)
+    {
+        if (itemDic.ContainsKey(i))
+        {
+
+            if (itemDic[i].num <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Exchange(Item i1, Item i2)
+    {
+        Add(i2);
+        Delete(i1);
+    }
+
+
+}
---
+++ CGJ2019/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+
+	public PackageManager.Item m_type;
+	public GameObject Icon;
+	public GameObject Door;
+	const int Player = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+	bool Check()
+	{
+		switch (m_type)
+		{
+			case PackageManager.Item.Axe:
+				return true;
+			case PackageManager.Item.apple:
+				return SingletonT<PackageManager>.Instance.Check(PackageManager.Item.Axe);
+			case PackageManager.Item.zhurou:
+				return SingletonT<PackageManager>.Instance.Check(PackageManager.Item.apple);
+			case PackageManager.Item.key:
+				return SingletonT<PackageManager>.Instance.Check(PackageManager.Item.zhurou);
+			case PackageManager.Item.piqiu:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	void Action()
+	{
+		if (!Check()) return;
+		switch (m_type)
+		{
+			case PackageManager.Item.Axe:
+				SingletonT<PackageManager>.Instance.Add(PackageManager.Item.Axe);
+				Destroy(gameObject);
+				return;
+			case PackageManager.Item.apple:
+				SingletonT<PackageManager>.Instance.Exchange(PackageManager.Item.Axe,PackageManager.Item.apple);
+				Destroy(gameObject);
+				return;
+			case PackageManager.Item.zhurou:
+				SingletonT<PackageManager>.Instance.Exchange(PackageManager.Item.apple, PackageManager.Item.zhurou);
+				Destroy(gameObject);
+				return;
+			case PackageManager.Item.key:
+				SingletonT<PackageManager>.Instance.Delete(PackageManager.Item.zhurou);
+				Door.transform.position = new Vector3(Door.transform.position.x,Door.transform.position.y + 3);
+				//Destroy(gameObject);
+				return;
+			case PackageManager.Item.piqiu:
+				SingletonT<PackageManager>.Instance.Add(PackageManager.Item.piqiu);
+				Destroy(gameObject);
+				return;
+			default:
+				return;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log(collision.gameObject.layer);
+		Debug.Log(Check());
+		if (collision.gameObject.layer == Player && Check())
+		{
+			Icon.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.layer == Player && Check())
+		{
+			Icon.SetActive(false);
+		}
+	}
+
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Action();
+			//switch ()
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
